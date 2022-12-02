716661b6d6f824eb3d745c481c6ed6ce88682d7a
Jose Vadell <me@jlvadell.com>
2022-12-02 12:58:17 +0100

    [DAY-1]: add day & separation prints

--- day1/src/run.py
+++ day1/src/run.py
@@ -1,7 +1,9 @@
 import main
 
+print("Day 1")
 data = main.load_input_data('../data/input.txt')
 print("Part One:")
 main.solve_p1(data)
 print("Part Two:")
 main.solve_p2(data)
+print("-----------------------------------------------------------")
