00a2b09ce9c59025378ffe9e0a88a067057ce101
Jose Vadell <me@jlvadell.com>
2022-12-07 19:28:28 +0100

    [DAY-5]: solve part one

--- day5/src/run.py
+++ day5/src/run.py
@@ -1,9 +1,10 @@
 import main
 
 print("Day 5")
-
+puzzle_input = '../data/input.txt'
 print("Part One:")
-
+solution_part_1 = main.solve_part_1(puzzle_input)
+print("Part 1 solution: '{0}'".format(solution_part_1))
 print("Part Two:")
 
 print("-----------------------------------------------------------")
