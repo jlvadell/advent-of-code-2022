0b5e92e0c4a79033dd7b408c76eec299efb98e28
Jose Vadell <me@jlvadell.com>
2022-12-07 12:19:49 +0100

    [DAY-4]: solve part 1

---
+++ day4/src/run.py
@@ -0,0 +1,9 @@
+import main
+
+print("Day 4")
+file ='../data/input.txt'
+print("Part One:")
+main.solve_part_1(file)
+print("Part Two:")
+
+print("-----------------------------------------------------------")
