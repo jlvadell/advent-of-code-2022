5e1856b7fd930f952ef2099be67217eebf1496e9
José Vadell <me@jlvadell.com>
2022-12-07 12:40:20 +0100

    [DAY-4]: Solution for day 4 (#4)
    * [DAY-3]: progress update
    
    * [DAY-4]: add Readme and test input
    
    * [DAY-4]: solve part 1
    
    * [DAY-4]: fix shadowing
    
    * [DAY-4]: add part 2 Readme
    
    * [DAY-4]: solve part 2
    
    * [DAY-4]: update progress

---
+++ day4/src/main.py
@@ -0,0 +1,36 @@
+def parse_assignments(raw_assignments: str):
+    assignments = raw_assignments.split(',')
+    return parse_assignment(assignments[0]), parse_assignment(assignments[1])
+
+
+def parse_assignment(assignments: str):
+    section_range = assignments.split('-')
+    return {x for x in range(int(section_range[0]), int(section_range[1])+1, 1)}
+
+
+def is_fully_overlapping(section: set, another_section: set):
+    return section.issubset(another_section) or another_section.issubset(section)
+
+def is_overlapping(section: set, another_section: set):
+    return bool(section & another_section)
+
+def load_data(input_file):
+    with open(input_file, 'r') as input_data:
+        data = []
+        for line in input_data.readlines():
+            data.append(parse_assignments(line))
+        return data
+
+def solve_part_1(input_file):
+    data = load_data(input_file)
+    total = 0
+    for sections in data:
+        total = total + is_fully_overlapping(sections[0], sections[1])
+    print("Total overlapping: {0}".format(total) )
+
+def solve_part_2(input_file):
+    data = load_data(input_file)
+    total = 0
+    for sections in data:
+        total = total + is_overlapping(sections[0], sections[1])
+    print("Total overlapping: {0}".format(total) )
---
+++ day4/tests/test.py
@@ -0,0 +1,79 @@
+import unittest
+from day4.src import main
+
+
+class Day4TestCase(unittest.TestCase):
+
+
+    def test_given_an_input_data__load_input_data__should_load_and_parse_data_correctly(self):
+        """
+            Given an input data, function 'load_data' should load and parse it into a list
+        """
+        # Given
+        inputs = 'data/test_input_1.txt'
+
+        # When
+        actual = main.load_data(inputs)
+        expected = [({2,3,4},{6,7,8}),({2,3},{4,5}),({5,6,7},{7,8,9}),({2,3,4,5,6,7,8},{3,4,5,6,7}),({6},{4,5,6}),({2,3,4,5,6},{4,5,6,7,8})]
+
+        # Then
+        self.assertEqual(expected, actual)  # add assertion here
+
+    def test_given_an_assignment__parse_assignment__should_return_set(self):
+        """
+            Given an assignment (start-end) should return set {start, ...., end}
+        """
+        # Given
+        assignment = '2-7'
+
+        # When
+        actual = main.parse_assignment(assignment)
+        expected = {2,3,4,5,6,7}
+
+        # Then
+        self.assertEqual(expected, actual)
+
+    def test_given_a_pair_of_assignments__parse_assignments__should_return_tuple_of_assignments(self):
+        """
+            Given a pair of assignments (start-end,start-end) function parse_assignments should return tuple with sets
+        """
+        # Given
+        assignments = '1-3,4-6'
+
+        # When
+        actual  = main.parse_assignments(assignments)
+        expected = ({1,2,3},{4,5,6})
+
+        # Then
+        self.assertEqual(expected, actual)
+
+    def test__is_fully_overlapping__should_return_true_if_one_set_overlaps_another(self):
+        """
+            Given 2 sets check if either set1 or set2 is subset of the other
+        """
+        # Given
+        params = [[{2,3,4}, {1,2,3,4,5}, 'Test case: set 1 is subset of set2'],
+                  [{1,2,3,4,5}, {2,3,4}, 'Test case: set 1 is superset of set2']]
+        for set1, set2, test_case in params:
+            # When
+            actual = main.is_fully_overlapping(set1, set2)
+
+            # Then
+            self.assertTrue(actual, test_case)
+
+    def test__is_overlapping__should_return_true_if_sets_have_common_items(self):
+        """
+            Given a pair of sets is_overlapping should return true if there are common items
+        """
+        # Given
+        set1 = {1,2,3,4,5,6}
+        set2 = {6,7,8,9,10}
+
+        # When
+        actual = main.is_overlapping(set1, set2)
+
+        # Then
+        self.assertTrue(actual)
+
+if __name__ == '__main__':
+    unittest.main()
