2486f7de3017694124d92c90a728aa8f47306a13
Jose Vadell <me@jlvadell.com>
2022-12-09 13:46:18 +0100

    [DAY-7]: add Readme + base files

---
+++ day7/src/run.py
@@ -0,0 +1,6 @@
+import main
+
+print("Day 7")
+print("Part One:")
+print("Part Two:")
+print("-----------------------------------------------------------")
---
+++ day7/tests/test.py
@@ -0,0 +1,9 @@
+import unittest
+from day7.src import main
+
+
+class Day7TestCase(unittest.TestCase):
+
+
+if __name__ == '__main__':
+    unittest.main()
