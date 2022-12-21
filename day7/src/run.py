89670babbdd93076e0e7a0450bdb6a3f787d4b21
Jose Vadell <me@jlvadell.com>
2022-12-21 12:20:06 +0100

    [DAY-7]: solve part 1

--- day7/src/run.py
+++ day7/src/run.py
@@ -1,6 +1,9 @@
 import main
 
 print("Day 7")
+input_file = '../data/input.txt'
 print("Part One:")
+solution = main.solve_part_1(input_file)
+print("Part 1 solution:", solution)
 print("Part Two:")
 print("-----------------------------------------------------------")
